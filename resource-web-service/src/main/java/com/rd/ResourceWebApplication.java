22197adfd6136fc104b9c5e2b345da6c71041821
mlarbo <touremohameda@gmail.com>
2018-01-26 15:59:47 +0100

    microservice version

---
+++ resource-web-service/src/main/java/com/rd/ResourceWebApplication.java
@@ -0,0 +1,12 @@
+package com.rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceWebApplication.class, args);
+    }
+}
